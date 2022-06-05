a0a4ab67f2e225215ef35b8f29163739f2262957
fady malak <fady.malak.awad@gmail.com>
2022-06-05 20:03:51 +0200

    Update 5PM

--- run_fabric.py
+++ run_fabric.py
@@ -2,10 +2,8 @@ from fabric import Connection
 import os
 import glob
 x = glob.glob("./web/templates/*.html")
-key = os.environ.get('KEY').split("\n")
-with open("priv2","w") as f:
-    f.writelines(key)
-c = Connection("34.125.213.99", port=22, user="fady", connect_kwargs={'look_for_keys': False,'key_filename':'priv2'})
+
+c = Connection("34.125.213.99", port=22, user="fady", connect_kwargs={'look_for_keys': False,'key_filename':'priv'})
 for i in x:
     out =i.split("/")[-1]
     c.put(f"{i}",f"/home/fady/soild/web/templates/{out}")
