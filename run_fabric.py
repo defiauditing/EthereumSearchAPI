75e1d216791485c48dd5b0ec0830a7b7dfdc446a
fady malak <fady.malak.awad@gmail.com>
2022-06-05 15:14:19 +0200

    add auto deploy fix

--- run_fabric.py
+++ run_fabric.py
@@ -5,7 +5,8 @@ x = glob.glob("./web/templates/*.html")
 print(x)
 c = Connection("34.125.213.99", port=22, user="fady", connect_kwargs={'look_for_keys': False,'key_filename':'priv'})
 for i in x:
-    c.put(f"{i}",f"/home/fady/soild/web/templates/{i}")
+    out =i.split("/")[-1]
+    c.put(f"{i}",f"/home/fady/soild/web/templates/{out}")
 res = c.run("sudo service uwsgi restart")
 res = c.run("sudo service nginx restart")
 print("Done")
