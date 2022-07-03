aaa3c86d08648e5d68680d0c22df15495d746df7
solidityaudit <96395333+solidityaudit@users.noreply.github.com>
2022-07-03 15:36:45 -0600

    Update run_fabric.py

--- run_fabric.py
+++ run_fabric.py
@@ -1,7 +1,7 @@
 from fabric import Connection 
 import os
 import glob
-x = glob.glob("./web/templates/*")
+x = glob.glob("./web/templates/*.html")
 c = Connection("34.125.213.99", port=22, user="fady", connect_kwargs={'look_for_keys': False,'key_filename':'priv'})
 for i in x:
     out =i.split("/")[-1]
