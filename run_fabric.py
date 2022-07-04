cebe2b81b6bfe6daf01e938489f11bb698a231b2
solidityaudit <96395333+solidityaudit@users.noreply.github.com>
2022-07-03 21:07:23 -0600

    Update run_fabric.py

--- run_fabric.py
+++ run_fabric.py
@@ -8,6 +8,7 @@ for i in x:
     c.put(f"{i}",f"/home/fady/soild/web/templates/{out}")
 res = c.run("sudo service uwsgi restart")
 res = c.run("sudo service nginx restart")
+res = c.run("sudo gcloud auth activate-service-account deployer --key-file=./key.json")
 res = c.run("sudo gcloud app deploy --log-http --verbosity=debug --impersonate-service-account=fiverr")
 print("Done")
 
