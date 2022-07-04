e31c9d3c9e67b4c42b58fff333050236213c5376
solidityaudit <96395333+solidityaudit@users.noreply.github.com>
2022-07-03 20:22:13 -0600

    Update run_fabric.py

--- run_fabric.py
+++ run_fabric.py
@@ -8,5 +8,5 @@ for i in x:
     c.put(f"{i}",f"/home/fady/soild/web/templates/{out}")
 res = c.run("sudo service uwsgi restart")
 res = c.run("sudo service nginx restart")
-res = c.run("sudo gcloud app deploy")
+res = c.run("sudo gcloud app deploy --log-http --verbosity=debug")
 print("Done")
