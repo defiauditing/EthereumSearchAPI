72ff5482dca2e4d4c866f661ec20444f70fa70fc
solidityaudit <96395333+solidityaudit@users.noreply.github.com>
2022-07-03 20:16:40 -0600

    Update run_fabric.py

--- run_fabric.py
+++ run_fabric.py
@@ -2,12 +2,11 @@ from fabric import Connection
 import os
 import glob
 x = glob.glob("./web/templates/*.html")
-c = Connection("34.125.213.99", port=22, user="fady", connect_kwargs={'look_for_keys': False,'key_filename':'priv'})
+c = Connection("34.125.213.99", port=22, user="fady", connect_kwargs={'look_for_keys': True,'key_filename':'priv'})
 for i in x:
     out =i.split("/")[-1]
     c.put(f"{i}",f"/home/fady/soild/web/templates/{out}")
 res = c.run("sudo service uwsgi restart")
 res = c.run("sudo service nginx restart")
-res = c.run("sudo gcloud config set fady")
 res = c.run("sudo gcloud deploy")
 print("Done")
